a203003443cd587ea610594127424458d1d5d510
gzjjjfree <68282570+gzjjjfree@users.noreply.github.com>
2022-08-25 17:35:32 +0800

    Update ngx_array.h

--- src/core/ngx_array.h
+++ src/core/ngx_array.h
@@ -5,7 +5,7 @@
  */
 
 
-#ifndef _NGX_ARRAY_H_INCLUDED_
+#ifndef _NGX_ARRAY_H_INCLUDED_      /* 防止ngx_array.h重复引入 */
 #define _NGX_ARRAY_H_INCLUDED_
 
 
@@ -14,18 +14,18 @@
 
 
 typedef struct {
-    void        *elts;
-    ngx_uint_t   nelts;
-    size_t       size;
-    ngx_uint_t   nalloc;
-    ngx_pool_t  *pool;
-} ngx_array_t;
+    void        *elts;               /* 定义无类型指针*elts */
+    ngx_uint_t   nelts;              /* 定义ngx_uint_t类型变量nelts */
+    size_t       size;               /* 定义size_t类型变量size */
+    ngx_uint_t   nalloc;             /* 定义ngx_uint_t类型变量nalloc */
+    ngx_pool_t  *pool;               /* 定义ngx_pool_t类型指针*pool */
+} ngx_array_t;                       /*ngx_array_t的数据结构*/
 
 
-ngx_array_t *ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size);
-void ngx_array_destroy(ngx_array_t *a);
-void *ngx_array_push(ngx_array_t *a);
-void *ngx_array_push_n(ngx_array_t *a, ngx_uint_t n);
+ngx_array_t *ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size);     /*定义ngx_array_create为ngx_array_t类型的指针（包含参数三个）*/
+void ngx_array_destroy(ngx_array_t *a);                                      /*定义无类型函数(参数为ngx_array_t型指针)*/
+void *ngx_array_push(ngx_array_t *a);                                        /*定义无类型函数指针(参数为ngx_array_t型指针)*/
+void *ngx_array_push_n(ngx_array_t *a, ngx_uint_t n);                        /*定义无类型函数指针(参数为ngx_array_t型指针，ngx_uint_t类型变量)*/
 
 
 static ngx_inline ngx_int_t
